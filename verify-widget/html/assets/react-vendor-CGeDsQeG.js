fcf91e1b240847dad1547a02763a624b58d188f5
Ahmet Abdullah Gultekin <ahmetabdullahgultekin@gmail.com>
2026-04-18 04:31:52 +0000

    chore(web-app): stop tracking generated verify-widget asset bundles

--- verify-widget/html/assets/react-vendor-CGeDsQeG.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./rolldown-runtime-COnpUsM8.js";import{lt as t}from"./mui-vendor-CKwTs-1b.js";var n=e((e=>{var n=t();e.createRoot=n.createRoot,e.hydrateRoot=n.hydrateRoot}));export{n as t};
